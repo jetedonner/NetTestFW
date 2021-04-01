fbfdf6118533e410ae54dc517aabe8f1b29f534f
jetedonner <kim@kimhauser.ch>
2021-04-01 23:58:03 +0200

    Initial Commit

---
+++ NetTestFW/NetTestFW.h
@@ -0,0 +1,18 @@
+//
+//  NetTestFW.h
+//  NetTestFW
+//
+//  Created by Kim David Hauser on 01.04.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetTestFW.
+FOUNDATION_EXPORT double NetTestFWVersionNumber;
+
+//! Project version string for NetTestFW.
+FOUNDATION_EXPORT const unsigned char NetTestFWVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetTestFW/PublicHeader.h>
+
+
